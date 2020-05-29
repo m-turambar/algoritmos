98a3a5295cc493cb67e06e10e8ed4aab7716a8aa
migue <miguel.lorenzo.1990@gmail.com>
2020-05-29 00:56:08 -0500

    fixing small typo

--- roulette_wheel.py
+++ roulette_wheel.py
@@ -8,5 +8,5 @@ wn = w/np.sum(w) #normalize the weight so that the sum of all weights equals 1
 suma = np.cumsum(wn) #perform a scan operation, that is, sum all preceding values of array at index i
 for i in range(N):
     r = random.uniform(0,1)
-    idx = np.searchsorted(s, r) # search would-be insertion spot for a sorted array - this will give us our individual to resample.
+    idx = np.searchsorted(suma, r) # search would-be insertion spot for a sorted array - this will give us our individual to resample.
     p3.append(p[idx]) # add that individual to our new array
